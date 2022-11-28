1c80ba0a469817bde8677de261d8d45c36a27c68
hyojeong <khj444453@gmail.com>
2022-11-28 19:12:30 +0900

    First Commit

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
